ea0ea7421301e347f62a2c57efd7c23e19da650f
marco <marco.maigua1346@gmail.com>
2019-07-24 19:46:32 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "marcomaigua.com/precache-manifest.ea01bfe289fb1f6b6434df66105c1ec0.js"
+  "/precache-manifest.6a5728ef8c21d635083b23ed742edcde.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("marcomaigua.com/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
