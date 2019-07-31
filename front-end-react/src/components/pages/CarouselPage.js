e8207bb40f7fd828bb92a51b6a42c6fc1887825d
marco <marco.maigua1346@gmail.com>
2019-07-30 20:51:12 -0600

    carousel page working

--- front-end-react/src/components/pages/CarouselPage.js
+++ front-end-react/src/components/pages/CarouselPage.js
@@ -14,12 +14,11 @@ class CarouselPage extends React.Component {
     render() {
         return (
 
-            <div className="home-page-container">
-                hola
-                <AwesomeSlider>
-                    <div data-src="/path/to/image-0.png" />
-                    <div data-src="/path/to/image-1.png" />
-                    <div data-src="/path/to/image-2.jpg" />
+            <div >
+                <AwesomeSlider >
+                    <div data-src="https://firebasestorage.googleapis.com/v0/b/cms-personal.appspot.com/o/photos%2Fprofessional%2FprofersionalImage1.jpg?alt=media&token=bf6b634c-00cd-43f3-9e7b-447607a3bcb7" />
+                    <div data-src="https://firebasestorage.googleapis.com/v0/b/cms-personal.appspot.com/o/photos%2Fprofessional%2FprofessionalImage2.jpg?alt=media&token=b3f34f15-7d1e-41d1-a4ef-41eba0d62bcf" />
+                    <div data-src="https://firebasestorage.googleapis.com/v0/b/cms-personal.appspot.com/o/photos%2Fprofessional%2FprofessionalImage3.jpg?alt=media&token=09ffbe8e-6602-4efb-93b1-a4476089536a" />
                 </AwesomeSlider>
             </div>
         );
