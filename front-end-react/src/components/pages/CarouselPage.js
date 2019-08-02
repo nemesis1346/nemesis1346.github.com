da630b76f5b3c430b0bf1709133e144c0001fd65
marco <marco.maigua1346@gmail.com>
2019-08-01 22:06:29 -0600

    carousel still not working

--- front-end-react/src/components/pages/CarouselPage.js
+++ front-end-react/src/components/pages/CarouselPage.js
@@ -1,92 +1,61 @@
 import React from "react";
 import { connect } from "react-redux";
 import {
-    getCarouselImages
+  getCarouselImages
 } from "../../actions/professionalFirebaseActions";
-import { SPANISH_LANGUAGE, ENGLISH_LANGUAGE } from '../../constants/types';
 import "../styles/homePage.css";
-import AwesomeSlider from 'react-awesome-slider';
-import 'react-awesome-slider/dist/styles.css';
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from 'react-responsive-carousel';
-import "react-image-gallery/styles/css/image-gallery.css";
-import ImageGallery from 'react-image-gallery';
+
+import { MDBCarousel, MDBCarouselCaption, MDBCarouselInner, MDBCarouselItem, MDBView, MDBMask, MDBContainer } from "mdbreact";
 
 class CarouselPage extends React.Component {
-    componentWillMount() {
-    }
+  componentWillMount() {
+  }
 
+  render() {
+    return (
+        <MDBContainer>
+          <MDBCarousel
+            activeItem={1}
+            length={3}
+            showControls={false}
+            showIndicators={false}
+            className="z-depth-1"
+            slide
+          >
+            <MDBCarouselInner>
+              <MDBCarouselItem itemId="1">
+                <MDBView>
+                  <img
+                    className="d-block w-100"
+                    src="https://firebasestorage.googleapis.com/v0/b/cms-personal.appspot.com/o/photos%2Fprofessional%2FprofersionalImage1.jpg?alt=media&token=bf6b634c-00cd-43f3-9e7b-447607a3bcb7"
+                    alt="First slide"
+                  />
+                </MDBView>
+              </MDBCarouselItem>
+              <MDBCarouselItem itemId="2">
+                <MDBView>
+                  <img
+                    className="d-block w-100"
+                    src="https://firebasestorage.googleapis.com/v0/b/cms-personal.appspot.com/o/photos%2Fprofessional%2FprofessionalImage2.jpg?alt=media&token=b3f34f15-7d1e-41d1-a4ef-41eba0d62bcf"
+                    alt="Second slide"
+                  />
+                </MDBView>
+              </MDBCarouselItem>
+              <MDBCarouselItem itemId="3">
+                <MDBView>
+                  <img
+                    className="d-block w-100"
+                    src="https://firebasestorage.googleapis.com/v0/b/cms-personal.appspot.com/o/photos%2Fprofessional%2FprofessionalImage3.jpg?alt=media&token=09ffbe8e-6602-4efb-93b1-a4476089536a"
+                    alt="Third slide"
+                  />
+                </MDBView>
+              </MDBCarouselItem>
+            </MDBCarouselInner>
+          </MDBCarousel>
+        </MDBContainer>
 
-    // render() {
-    //     return (
-
-    //         <div >
-    //             <AwesomeSlider >
-    //                 <div data-src="https://firebasestorage.googleapis.com/v0/b/cms-personal.appspot.com/o/photos%2Fprofessional%2FprofersionalImage1.jpg?alt=media&token=bf6b634c-00cd-43f3-9e7b-447607a3bcb7" >HOLA A TODOS</div>
-    //                 <div data-src="https://firebasestorage.googleapis.com/v0/b/cms-personal.appspot.com/o/photos%2Fprofessional%2FprofessionalImage2.jpg?alt=media&token=b3f34f15-7d1e-41d1-a4ef-41eba0d62bcf" />
-    //                 <div data-src="https://firebasestorage.googleapis.com/v0/b/cms-personal.appspot.com/o/photos%2Fprofessional%2FprofessionalImage3.jpg?alt=media&token=09ffbe8e-6602-4efb-93b1-a4476089536a" />
-    //             </AwesomeSlider>
-    //         </div>
-    //     );
-    // }
-
-    // render() {
-    //     return (
-    //         <Carousel>
-    //             <div>
-    //                 <img src="https://firebasestorage.googleapis.com/v0/b/cms-personal.appspot.com/o/photos%2Fprofessional%2FprofersionalImage1.jpg?alt=media&token=bf6b634c-00cd-43f3-9e7b-447607a3bcb7" />
-    //                 <p className="legend">Legend 1</p>
-    //             </div>
-    //             <div>
-    //                 <img src="https://firebasestorage.googleapis.com/v0/b/cms-personal.appspot.com/o/photos%2Fprofessional%2FprofessionalImage2.jpg?alt=media&token=b3f34f15-7d1e-41d1-a4ef-41eba0d62bcf" />
-    //                 <p className="legend">Legend 2</p>
-    //             </div>
-    //             <div>
-    //                 <img src="https://firebasestorage.googleapis.com/v0/b/cms-personal.appspot.com/o/photos%2Fprofessional%2FprofessionalImage3.jpg?alt=media&token=09ffbe8e-6602-4efb-93b1-a4476089536a" />
-    //                 <p className="legend">Legend 3</p>
-    //             </div>
-    //         </Carousel>
-    //     );
-    // }
-
-    render() {
-        const images = [
-            {
-              original: "https://firebasestorage.googleapis.com/v0/b/cms-personal.appspot.com/o/photos%2Fprofessional%2FprofersionalImage1.jpg?alt=media&token=bf6b634c-00cd-43f3-9e7b-447607a3bcb7",
-              originalTitle:"TEST"
-            },
-            {
-              original: "https://firebasestorage.googleapis.com/v0/b/cms-personal.appspot.com/o/photos%2Fprofessional%2FprofessionalImage2.jpg?alt=media&token=b3f34f15-7d1e-41d1-a4ef-41eba0d62bcf",
-
-            },
-            {
-              original: "https://firebasestorage.googleapis.com/v0/b/cms-personal.appspot.com/o/photos%2Fprofessional%2FprofessionalImage3.jpg?alt=media&token=09ffbe8e-6602-4efb-93b1-a4476089536a",
-
-            }
-          ]
-        return (
-            <ImageGallery items={images}   showThumbnails={false}>
-                hola a TODOS
-            </ImageGallery>
-
-        );
-    }
+    );
+  }
 }
-
-const mapStateToPropsHomePage = state => {
-    //In this case objects is gonna be applied to the props of the component
-    return {
-        professionalProfilePhoto:
-            state.professionalPageReducer.professionalProfilePhoto,
-    };
-};
-
-// export default connect(
-//   mapStateToPropsHomePage,
-//   {
-//     getProfessionalImage,
-//   }
-// )(HomePage);
-
 
 export default CarouselPage;
