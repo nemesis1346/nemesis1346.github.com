7bd3bfbdc0f749628f024dab05377c91da13913d
nemesis1346 <marco.maigua1346@gmail.com>
2024-10-09 09:49:18 -0400

    dockers workign

--- front-end-react/src/index.js
+++ front-end-react/src/index.js
@@ -1,16 +1,19 @@
+// React libraries:
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/rootReducer';
 import './index.css';
 import App from './App';
+
+// Firebase libraries
 import { reduxFirestore, getFirestore } from 'redux-firestore';
 import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
 import FirestoreConfig from './api/FirebaseConfig';
 import FirebaseConfig from './api/FirebaseConfig';
 import "semantic-ui-css/semantic.min.css";
-import { createStore, applyMiddleware, compose } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers/rootReducer';
 
 const store = createStore(
     rootReducer,
