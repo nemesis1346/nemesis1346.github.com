f850d8d953d45056569da42acad4aaf0b55681ac
Tony Maigua <usuario@192.168.0.102>
2024-11-01 20:59:24 -0500

    unnenesary files and env file

--- front-end-react/src/api/FirebaseConfig.js
+++ front-end-react/src/api/FirebaseConfig.js
@@ -1,13 +1,13 @@
 // TODO: I need to find a way to make this private otherwise is a breach to security
 //Just firebase
 export const FirebaseConfig = {
-  apiKey: "AIzaSyApSjCi7NF-Nw26drJJunygCLaSwf8l4Bo",
-  authDomain: "cms-personal.firebaseapp.com",
-  databaseURL: "https://cms-personal.firebaseio.com",
-  projectId: "cms-personal",
-  storageBucket: "cms-personal.appspot.com",
-  messagingSenderId: "524767534957",
-  appId: "1:524767534957:web:88a0ef38e500595789c464"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 
